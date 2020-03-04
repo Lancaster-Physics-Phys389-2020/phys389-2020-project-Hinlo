b761cea00834fe6ebae4f46d02b265f849799cc6
Hinlo <57256469+Hinlo@users.noreply.github.com>
2020-03-04 13:05:28 +0000

    389 Week 3.1
    Working: 
    - Test file for a given proton for all supporting functions to atmosphere function and for atmosphere function with a stationary proton.

--- 389 Week 3/AtmosphereFinal.py
+++ 389 Week 3/AtmosphereFinal.py
@@ -6,43 +6,12 @@ from scipy import constants
 
 from Particle import Particle
 from Particle import Charticle
+from AtmosphereSupportFunctions import StoppingForce, NonRelativisticStoppingForce, Direction, ForceDirectionCheck
 
-Proton = Charticle([0,2.5E4,0], [2.3E7,-2.5E7,0], [0,0,0],'Proton', 1.67E-27, 1)
-
-def StoppingForce(eDensity,charge,beta,V_excitation): # Note beta is different for each x, y, z direction
-    t1 = (4*math.pi)/(constants.electron_mass*constants.speed_of_light*constants.speed_of_light)
-    t2 = (eDensity*charge*charge)/(beta*beta)
-    t3 = (constants.elementary_charge*constants.elementary_charge)/(4*math.pi*constants.epsilon_0)
-    ln = np.log((2*constants.electron_mass*constants.speed_of_light*constants.speed_of_light*beta*beta)/(V_excitation*(1-beta*beta)))
-    t4 = ln - beta*beta
-    StoppingForce = -t1*t2*t3*t3*t4
-    return(StoppingForce)
-
-def NonRelativisticStoppingForce(eDensity,charge,ispeed,V_excitation):
-    t1 = (4*math.pi*eDensity*charge*charge)/(constants.electron_mass*ispeed*ispeed)
-    t2 = (constants.elementary_charge*constants.elementary_charge)/(4*math.pi*constants.epsilon_0)
-    t3 = np.log((2*constants.electron_mass*ispeed*ispeed)/(V_excitation))
-    NonRelStoppingForce = -t1*t2*t2*t3
-    return(NonRelStoppingForce)
-
-def Direction(VelocityVector):
-    DirectionList = []
-    for i in range(len(VelocityVector)):
-        if VelocityVector[i] >= 0:
-            DirectionList.append(1)
-        else:
-            DirectionList.append(-1)
-    return(DirectionList)
-    
-def ForceDirectionCheck(DirectionListComponent,AccelerationComponent):
-    if DirectionListComponent >= 0:
-        return(AccelerationComponent)
-    else: 
-        return(-AccelerationComponent)
+Proton = Charticle([0,2.5E4,0], [2.7E8,-2.5E8,0], [0,0,0],'Proton', 1.67E-27, 1)
 
 def Atmosphere(CosmicRay,RunTime):
     
-    #Initialise values
     time = 0.0
     deltaT = 0.01*RunTime
     PositionList = [[],[],[]]
@@ -57,7 +26,6 @@ def Atmosphere(CosmicRay,RunTime):
         beta = CosmicRay.beta()
 
         for i in range(len(CosmicRay.velocity)):
-            print('x = 0, y = 1, i =',i)
             PositionList[i].append(CosmicRay.position[i])
             VelocityList[i].append(CosmicRay.velocity[i])
             if np.abs(CosmicRay.velocity[i]) <= 2.7E6:
@@ -65,19 +33,14 @@ def Atmosphere(CosmicRay,RunTime):
 
             else:
                 if np.abs(CosmicRay.velocity[i]) >= 1E7:
-                    Force = StoppingForce(2.5E25,CosmicRay.charge,beta[i],1.29E-17)
-                    print('relativistic!')
+                    Force = StoppingForce(2.5E25,CosmicRay.charge,beta[i],1.29E-17)                 
                 else:
-                    Force = NonRelativisticStoppingForce(2.5E25,CosmicRay.charge,CosmicRay.velocity[i],1.29E-17)
-                    print('Slow Boi!')
+                    Force = NonRelativisticStoppingForce(2.5E25,CosmicRay.charge,CosmicRay.velocity[i],1.29E-17)                    
 
                 AccelerationCalc = (Force/CosmicRay.mass)
-                print(AccelerationCalc)
-                CosmicRay.acceleration[i] = ForceDirectionCheck(ListofDirections[i],AccelerationCalc)
-                print(CosmicRay.acceleration[i])
+                CosmicRay.acceleration[i] = ForceDirectionCheck(ListofDirections[i],AccelerationCalc)                
                 AccerlerationList[i].append(CosmicRay.acceleration[i])
                 CosmicRay.Velocityupdate(i,deltaT)
-                print(CosmicRay.velocity)
                 
                 if ListofDirections[i] == 1 and CosmicRay.velocity[i] < 0:
                     CosmicRay.velocity[i] = 0
@@ -96,6 +59,7 @@ def Atmosphere(CosmicRay,RunTime):
                      'X Acceleration':AccerlerationList[0], 'Y Acceleration':AccerlerationList[1], 'Z Acceleration':AccerlerationList[2]}
     CosmicRayDataFrame = pd.DataFrame(CosmicRayData)
     CosmicRayDataFrame.to_pickle('Cosmic_Ray_Data.csv')
+    return(CosmicRay)
     
 
-Atmosphere(Proton, 0.000001)
+Atmosphere(Proton, 0.0001)
--- 389 Week 3/test_CosmicRay.py
+++ 389 Week 3/test_CosmicRay.py
@@ -3,31 +3,36 @@ import pandas as pd
 import math
 import copy
 from scipy import constants
+from pytest import approx
 
 from Particle import Particle
 from Particle import Charticle
-from AtmosphereFinal import StoppingForce, Direction, ForceDirectionCheck, Atmosphere
-
+from AtmosphereSupportFunctions import StoppingForce,NonRelativisticStoppingForce, Direction, ForceDirectionCheck
+from AtmosphereFinal import Atmosphere
 
 # To test: do pytest for each function in AtmosphereFinal, small tests to assert that each gives the expected value. With atmosphere function, calculate/find a database with the values we expect after the first time step of in the data frame, assert that these correct values equal those of the dataframe given by python.
 # To initialise test: open terminal: view -> Terminal, once terminal loads, type in pytest, this runs pytest across all files in the folder. Currently not running my test. Fix this.
 TestProton = Charticle([0,2.5E4,0], [2.3E7,-2.5E7,0], [0,0,0],'Proton', 1.67E-27, 1)
+StationaryProton = Charticle([0,2.5E4,0], [0,0,0], [0,0,0],'Proton', 1.67E-27, 1)
 
 
 
 
+def test_Stoppingforce(): # check that relativistic stopping force gives correct value for a given input
+    assert StoppingForce(2.5E25,1,0.9,1.29E-17) == approx(-2.5E-15)
 
-# def test_Stoppingforce():
-#     assert
+def test_NonRelativisticStoppingforce(): # check that non-relativistic stopping force gives correct value for a given input
+    assert NonRelativisticStoppingForce(2.5E25,1,5E6,1.29E-17) == approx(-9.2E-13)
 
-def test_Direction ():
+def test_Direction (): # check that particles direction is correctly identified
     assert Direction(TestProton.velocity) == [1,-1,1]
 
-def test_ForceDirectionCheck():
+def test_ForceDirectionCheck(): # check that forcedirection is only switched if particle moves in the negative direction along a given axis
     assert ForceDirectionCheck(1,1) == 1
     assert ForceDirectionCheck(-1,1) == -1
 
-# def test_Atmosphere():
-#     assert Atmosphere(TestProton, 
+def test_Atmosphere(): # check particles with no initial velocity or acceleration are unchanged
+    assert Atmosphere(StationaryProton, 0.001) == StationaryProton
+
 
 
