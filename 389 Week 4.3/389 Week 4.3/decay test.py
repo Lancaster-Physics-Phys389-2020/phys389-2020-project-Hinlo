6ef7b24bed118f4d6319cb18b43f36a00d06a32a
Hinlo <57256469+Hinlo@users.noreply.github.com>
2020-03-12 10:24:30 +0000

    389 Week 4.3
    - Fixed issue in appending wrong data to data frames

---
+++ 389 Week 4.3/389 Week 4.3/decay test.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import math
+import copy
+from scipy import constants
+import random
+
+
+print(random.randint(1,10))
+pd.options.display.max_rows = 150
+print(pd.read_pickle('Data/Cosmic_Ray_Data_3.csv'))
